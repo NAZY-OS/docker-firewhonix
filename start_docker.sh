582c4378497539b69a6364a057e81f515c10cc19
~ NAZY-OS ~ CodeSpace <114874290+NAZY-OS@users.noreply.github.com>
2026-08-04 09:11:34 +0000

    Bump to v1.2
    Update start_docker.sh

--- start_docker.sh
+++ start_docker.sh
@@ -7,19 +7,22 @@ EXTERNAL_PORT_4711=4711
 EXTERNAL_PORTS_9051_9062="9051-9062"
 EXTERNAL_PORT_9080=9080
 
+#Create my_vlan
+docker network create my_vlan 2> /dev/null
+
 # Starte den Docker-Container
 if [[ "$response" == "y" || "$response" == "Y" ]]; then
     docker run --rm -it \
         -p $EXTERNAL_PORT_4711:$EXTERNAL_PORT_4711 \
         -p $EXTERNAL_PORTS_9051_9062:$EXTERNAL_PORTS_9051_9062 \
         -p $EXTERNAL_PORT_9080:$EXTERNAL_PORT_9080 \
-        --network host \
-        firewhonix:1.1 /bin/sh -c "echo 'Container started' && ls -lha && sh -c 'start.sh &';bash"
+        --network my_vlan \
+        firewhonix:1.2 /bin/sh -c "echo 'Container started' && ls -lha && sh -c 'start.sh &';bash"
 else
     docker run --rm \
         -p $EXTERNAL_PORT_4711:$EXTERNAL_PORT_4711 \
         -p $EXTERNAL_PORTS_9051_9062:$EXTERNAL_PORTS_9051_9062 \
         -p $EXTERNAL_PORT_9080:$EXTERNAL_PORT_9080 \
-        --network host \
-        firewhonix:1.1 /bin/sh -c "echo 'Container started' && ls -lha && sh -c 'start.sh &';bash"
+        --network my_vlan \
+        firewhonix:1.2 /bin/sh -c "echo 'Container started' && ls -lha && sh -c 'start.sh &';bash"
 fi
