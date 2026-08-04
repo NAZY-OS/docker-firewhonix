acc3d2857d933db781ab5ee416ca6352eb6f5a43
~ NAZY-OS ~ CodeSpace <114874290+NAZY-OS@users.noreply.github.com>
2026-08-04 09:27:45 +0000

    Fix vlan
    Update start_docker.sh

--- start_docker.sh
+++ start_docker.sh
@@ -8,7 +8,7 @@ EXTERNAL_PORTS_9051_9062="9051-9062"
 EXTERNAL_PORT_9080=9080
 
 #Create my_vlan
-docker network create my_vlan 2> /dev/null
+#docker network create my_vlan 2> /dev/null
 
 # Starte den Docker-Container
 if [[ "$response" == "y" || "$response" == "Y" ]]; then
@@ -16,13 +16,13 @@ if [[ "$response" == "y" || "$response" == "Y" ]]; then
         -p $EXTERNAL_PORT_4711:$EXTERNAL_PORT_4711 \
         -p $EXTERNAL_PORTS_9051_9062:$EXTERNAL_PORTS_9051_9062 \
         -p $EXTERNAL_PORT_9080:$EXTERNAL_PORT_9080 \
-        --network my_vlan \
+        --network host \
         firewhonix:1.2 /bin/sh -c "echo 'Container started' && ls -lha && sh -c 'start.sh &';bash"
 else
     docker run --rm \
         -p $EXTERNAL_PORT_4711:$EXTERNAL_PORT_4711 \
         -p $EXTERNAL_PORTS_9051_9062:$EXTERNAL_PORTS_9051_9062 \
         -p $EXTERNAL_PORT_9080:$EXTERNAL_PORT_9080 \
-        --network my_vlan \
+        --network hist \
         firewhonix:1.2 /bin/sh -c "echo 'Container started' && ls -lha && sh -c 'start.sh &';bash"
 fi
